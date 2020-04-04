42f0e5a264743f1ddea44819619299b33e07dfb8
bdvllrs <bdvllrs@gmail.com>
2020-04-04 11:35:56 +0200

    Add test for routing by actions.

--- tests/test_state_manager.py
+++ tests/test_state_manager.py
@@ -20,6 +20,9 @@ def init_states():
 
     mid1 = StateNode("mid1")
     mid2 = StateNode("mid2")
+
+    stage_mng.add_states([start_state, end_state, mid1, mid2])
+
     return stage_mng, start_state, end_state, mid1, mid2
 
 
@@ -27,6 +30,7 @@ def test_state_manager():
     stage_mng, start_state, end_state, mid1, mid2 = init_states()
 
     start_state.add_edge(mid1)
+    # Conditions are checked BEFORE actions are executed.
     mid1.add_edge(mid1,
                   condition=lambda env: env['counter'] < 2,
                   actions=[add_one_counter_action])
@@ -34,7 +38,6 @@ def test_state_manager():
                   condition=lambda env: env['counter'] >= 2)
     mid2.add_edge(end_state)
 
-    stage_mng.add_states([start_state, end_state, mid1, mid2])
     stage_mng['counter'] = 0
 
     state_order = []
@@ -54,7 +57,6 @@ def test_state_else_condition():
     mid1.add_edge(mid2)  # If no condition, will be used as an "else" condition
     mid2.add_edge(end_state)
 
-    stage_mng.add_states([start_state, end_state, mid1, mid2])
     stage_mng['counter'] = 0
 
     state_order = []
@@ -74,7 +76,6 @@ def test_state_two_else_condition():
     mid1.add_edge(mid2)
     mid2.add_edge(end_state)
 
-    stage_mng.add_states([start_state, end_state, mid1, mid2])
     stage_mng['counter'] = 0
 
     state_order = []
@@ -94,7 +95,6 @@ def test_change_routing_in_action():
     mid1.add_edge(mid2)  # If no condition, will be used as an "else" condition
     mid2.add_edge(end_state)
 
-    stage_mng.add_states([start_state, end_state, mid1, mid2])
     stage_mng['counter'] = 0
 
     state_order = []
@@ -103,3 +103,27 @@ def test_change_routing_in_action():
 
     assert state_order == ["start", "mid1", "mid2", "end"]
     assert stage_mng['counter'] == 1
+
+
+def route_mid1(node, env):
+    if env['counter'] < 2:
+        node.next_node = 'mid1'
+    else:
+        node.next_node = 'mid2'
+
+
+def test_route_by_actions():
+    stage_mng, start_state, end_state, mid1, mid2 = init_states()
+    start_state.add_edge(mid1)
+    # Use end_state as fallback state if no other route has been chosen in actions.
+    mid1.add_edge(end_state, actions=[route_mid1, add_one_counter_action])
+    mid2.add_edge(end_state)
+
+    stage_mng['counter'] = 0
+
+    state_order = []
+    for state in stage_mng():
+        state_order.append(state.name)
+
+    assert state_order == ["start", "mid1", "mid1", "mid1", "mid2", "end"]
+    assert stage_mng['counter'] == 3
