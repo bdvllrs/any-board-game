f37284fe76c0ad9f6896a15485b8a684388cb616
bdvllrs <bdvllrs@gmail.com>
2020-05-10 10:34:38 +0200

    Add yaml and markdown files in package data.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 setup(name='game-engine',
       version='0.1.0',
       install_requires=['PyYAML', 'aiohttp', 'numpy'],
+      package_data={
+        "": ['*.sh'],
+        "game_engine.games": ['*.yaml', '*.md']
+      },
       extras_require={
           'dev': [
               'pytest',
