828ba8bf1fcf61362e5df3493ce9c4dda39a237b
bdvllrs <bdvllrs@gmail.com>
2020-05-10 10:44:48 +0200

    Add all files in game_engine/games as package data.

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import os
+
 from setuptools import find_packages, setup
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
 
 setup(name='game-engine',
       version='0.1.0',
       install_requires=['PyYAML', 'aiohttp', 'numpy'],
       package_data={
-        "": ['*.sh'],
-        "game_engine.games": ['*.yaml', '*.md']
+          "": package_files("game_engine/games")
       },
       extras_require={
           'dev': [
